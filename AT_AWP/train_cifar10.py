16ac0b6574ce125c95e2509ca852ab5e034038f2
cardistryj <547207686@qq.com>
2022-12-10 13:57:51 +0800

    opt: readme & dir

--- AT_AWP/train_cifar10.py
+++ AT_AWP/train_cifar10.py
@@ -134,7 +134,7 @@ def get_args():
     parser.add_argument('--l1', default=0, type=float)
     parser.add_argument('--batch-size', default=128, type=int)
     parser.add_argument('--batch-size-test', default=128, type=int)
-    parser.add_argument('--data-dir', default='./cifar-data', type=str)
+    parser.add_argument('--data-dir', default='./data/cifar-data', type=str)
     parser.add_argument('--epochs', default=200, type=int)
     parser.add_argument('--lr-schedule', default='piecewise', choices=['superconverge', 'piecewise', 'linear', 'piecewisesmoothed', 'piecewisezoom', 'onedrop', 'multipledecay', 'cosine', 'cyclic'])
     parser.add_argument('--lr-max', default=0.1, type=float)
@@ -199,6 +199,8 @@ def main():
     if args.awp_gamma <= 0.0:
         args.awp_warmup = np.infty
 
+    # redirect output to ./output directory
+    args.fname = os.path.join('./output', args.fname)
     if not os.path.exists(args.fname):
         os.makedirs(args.fname)
 
--- README.md
+++ README.md
@@ -2,4 +2,15 @@ Code repository for model weights robustifying research
 
 Base on the official code from AWP
 Paper (NeurIPS 2020 "[Adversarial Weight Perturbation Helps Robust Generalization](https://arxiv.org/pdf/2004.05884.pdf)").
-Github link https://github.com/csdongxian/AWP
+Github link https://github.com/csdongxian/AWP.
+
+## Cifar-10 Dataset
+#### Setting 1
+Training attack pgd-10 with L2 norm, epsilon 128/255, and step-size (pgd-alpha) 15/255. 
+Example command as follows
+`python AT-AWP/train_cifar10.py --norm l_2 --pgd-alpha 15 --epsilon 128`
+
+#### Setting 2
+Training attack pgd-10 with Linf norm, epsilon 8/255, and step-size (pgd-alpha) 2/255. 
+Example command as follows
+`python AT-AWP/train_cifar10.py --norm l_inf --pgd-alpha 2 --epsilon 8`
